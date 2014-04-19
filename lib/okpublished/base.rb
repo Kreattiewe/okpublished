e2d4739e639f292bde43cbca1d8822aa5fc6cb11
Angel Botto <angelbotto@gmail.com>
2014-04-18 21:22:46 -0500

    added debug mode

--- lib/okpublished/base.rb
+++ lib/okpublished/base.rb
@@ -7,23 +7,26 @@ module Okpublished
       @config = config
     end
 
-    def publicate_facebook(text)
-      graph = Koala::Facebook::GraphAPI.new(@config[:facebook][:access_token])
+    def publicate_facebook(text, image)
+      
+      url_to_file(image)
+      graph = Koala::Facebook::API.new(@config[:facebook][:access_token])
       page_token = graph.get_page_access_token(@config[:facebook][:page_id])
 
-      page = Koala::Facebook::API.new(page_token)
-      page.put_picture(File.new("/tmp/image.jpg"), { :message => text})
+      @page = Koala::Facebook::API.new(page_token)
+      @page.put_picture(File.new("/tmp/image.jpg"), { :message => text})
     end
 
-    def publicate_twitter(text)
-      client = Twitter::REST::Client.new do |config|
+    def publicate_twitter(text, image)
+      url_to_file(image)
+      @client = Twitter::REST::Client.new do |config|
         config.consumer_key        = @config[:twitter][:consumer_key]
         config.consumer_secret     = @config[:twitter][:consumer_secret]
         config.access_token        = @config[:twitter][:access_token]
         config.access_token_secret = @config[:twitter][:access_token_secret]
       end
 
-      client.update_with_media("#{text}", File.new("/tmp/image.jpg"))
+      @client.update_with_media("#{text}", File.new("/tmp/image.jpg"))
     end
     
     def url_to_file(url)
@@ -41,15 +44,17 @@ module Okpublished
     end
 
 
-    def publicate(text, image)
-      url_to_file(image)
+    def publicate(text, image,debug=false)
+      if debug
+        puts "#{text} - #{image}"
+      end
 
       if validate_facebook?
-        publicate_facebook(text)
+        publicate_facebook(text, image)
       end
 
       if validate_twitter?
-        publicate_twitter(text)
+        publicate_twitter(text, image)
       end
     end
 
